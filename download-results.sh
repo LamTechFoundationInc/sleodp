d0b532b123893047f50d5c3e96d8732af0ca2e51
Tamba Lamin <tamba@tambalamin.me>
2018-03-07 08:43:07 -0500

    updated presidential election results for 2018 in src and www

--- download-results.sh
+++ download-results.sh
@@ -28,40 +28,50 @@ fpollingcentre=/Users/tamba.s.lamin/DEV/sloedp/src/assets/resources/polling-cent
 ppollingcentre=
 
 #Download Presidential results
-wget -o $fpresident/all-president-polling-centre-results-01.json $ppresident/all-president-polling-centre-results-01.json
-wget -o $fpresident/all-president-polling-centre-results-02.json $ppresident/all-president-polling-centre-results-02.json
-wget -o $fpresident/all-president-polling-centre-results-03.json $ppresident/all-president-polling-centre-results-03.json
-wget -o $fpresident/all-president-polling-centre-results-04.json $ppresident/all-president-polling-centre-results-04.json
+cd /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-president-polling-centre-results-2018
+rm -rf all-president-polling-centre-results-*.json
+wget $ppresident/all-president-polling-centre-results-01.json
+wget $ppresident/all-president-polling-centre-results-02.json
+wget $ppresident/all-president-polling-centre-results-03.json
+wget $ppresident/all-president-polling-centre-results-04.json
+#wget -o $fpresident/all-president-polling-centre-results-01.json $ppresident/all-president-polling-centre-results-01.json
+#wget -o $fpresident/all-president-polling-centre-results-02.json $ppresident/all-president-polling-centre-results-02.json
+#wget -o $fpresident/all-president-polling-centre-results-03.json $ppresident/all-president-polling-centre-results-03.json
+#wget -o $fpresident/all-president-polling-centre-results-04.json $ppresident/all-president-polling-centre-results-04.json
 
 #Download Parliamentary results
-wget -o $fparliamentary/all-parliamentary-polling-centre-results-01.json $pparliamentary/all-parliamentary-polling-centre-results-01.json
-wget -o $fparliamentary/all-parliamentary-election-results.json $pparliamentary/all-parliamentary-election-results.json
+#wget -o $fparliamentary/all-parliamentary-polling-centre-results-01.json $pparliamentary/all-parliamentary-polling-centre-results-01.json
+#wget -o $fparliamentary/all-parliamentary-election-results.json $pparliamentary/all-parliamentary-election-results.json
 
 #Download Mayor/Chair results
-wget -o $fmayorchairperson/all-mayor-chair-polling-centre-results-01.json $pmayorchairperson/all-mayor-chair-polling-centre-results-01.json
-wget -o $fmayorchairperson/all-councillor-election-results.json $pmayorchairperson/all-councillor-election-results.json
+#wget -o $fmayorchairperson/all-mayor-chair-polling-centre-results-01.json $pmayorchairperson/all-mayor-chair-polling-centre-results-01.json
+#wget -o $fmayorchairperson/all-major-chair-election-results.json $pmayorchairperson/all-major-chair-election-results.json
 
 #Download Councillor results
-wget -o $fcouncillor/all-councillor-polling-centre-results-01.json $pcouncillor/all-councillor-polling-centre-results-01.json
+#wget -o $fcouncillor/all-councillor-polling-centre-results-01.json $pcouncillor/all-councillor-polling-centre-results-01.json
+#wget -o $fmayorchairperson/all-councillor-election-results.json $pmayorchairperson/all-councillor-election-results.json
 
 #Download Polling Centres
-all-polling-centres-02.json
-all-polling-centres-01.json
+#all-polling-centres-02.json
+#all-polling-centres-01.json
 
 #Download Candidates
-all-candidates.json
+#all-candidates.json
 
 #Download Political Parties
-all-political-parties.json
+#all-political-parties.json
 
 #Download Wards
-all-wards.json
+#all-wards.json
 
 #Download constituencies
-all-constituency.json
+#all-constituency.json
 
 #Download Districts
-all-district.json
+#all-district.json
 
 #Download Regions
-all-regions.json
+#all-regions.json
+
+#wget -o /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-president-polling-centre-results-2018/all-president-polling-centre-results-01.json https://electiondata.io/api/results/presidential/all-president-polling-centre-results-01.json
+#wget https://electiondata.io/api/results/presidential/all-president-polling-centre-results-01.json
